09b7a9dbc7ba75480d82c1fd3d80ccc46300c332
Gabe Parra <gabe.parra@cerner.com>
2019-02-08 13:18:41 -0600

    Added required prop and example. Created 2 local CSS classes. Updated Jest snapshots

---
+++ packages/terra-date-picker/src/terra-dev-site/test/date-picker/DatePickerRequired.test.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import DatePicker from '../../../../lib/DatePicker';
+
+const DatePickerRequired = () => (
+  <DatePicker
+    name="date-input"
+  />
+);
+
+export default DatePickerRequired;
